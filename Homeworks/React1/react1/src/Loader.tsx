c4ae4d85945e585137fa718dd2187757acac539f
Lucas Garcia <lucasgarcia@uao.edu.co>
2026-02-14 19:23:14 -0500

    Finish Challenge 2

---
+++ Homeworks/React1/react1/src/Loader.tsx
@@ -0,0 +1,5 @@
+function Loader() {
+  return <h2>Cargando contactos...</h2>;
+}
+
+export default Loader;
--- Homeworks/React1/react1/src/main.tsx
+++ Homeworks/React1/react1/src/main.tsx
@@ -1,11 +1,75 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import ReactDOM from "react-dom/client";
-import PrintMessage from "./PrintMessage";
 
+import Loader from "./loader";
+import ContactList from "./ListaContactos";
+import AddContact from "./AddContacto";
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
-    <React.StrictMode>
-        <PrintMessage message = 'Lista de Contactos'/>
-    </React.StrictMode>
-)
+interface Contact {
+  id: number;
+  name: string;
+  phone: string;
+}
 
+function Main() {
+
+  const [contacts, setContacts] = useState<Contact[]>([]);
+  const [loading, setLoading] = useState(true);
+
+  // Simulación carga inicial
+  useEffect(() => {
+
+    setTimeout(() => {
+      setContacts([
+        { id: 1, name: "Juan", phone: "123456" },
+        { id: 2, name: "Ana", phone: "987654" }
+      ]);
+
+      setLoading(false);
+
+    }, 2000);
+
+  }, []);
+
+  const addContact = (name: string, phone: string) => {
+    const newContact: Contact = {
+      id: Date.now(),
+      name,
+      phone
+    };
+
+    setContacts(prev => [...prev, newContact]);
+  };
+
+  const deleteContact = (id: number) => {
+    setContacts(prev =>
+      prev.filter(contact => contact.id !== id)
+    );
+  };
+
+  return (
+    <>
+      <h1>Contact List</h1>
+
+      {
+        loading
+          ? <Loader />
+          : <>
+              <AddContact onAdd={addContact} />
+              <ContactList
+                contacts={contacts}
+                onDelete={deleteContact}
+              />
+            </>
+      }
+    </>
+  );
+}
+
+ReactDOM.createRoot(
+  document.getElementById("root") as HTMLElement
+).render(
+  <React.StrictMode>
+    <Main />
+  </React.StrictMode>
+);
